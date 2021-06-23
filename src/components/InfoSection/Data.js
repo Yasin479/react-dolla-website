1d6971a956a9743eafacedf6c518179f3af1ded4
Yasin Oeztuerk <yasinoeztuerk02gmail.com>
2021-06-23 22:28:17 +0200

    First version of the website

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,50 @@
+export const homeObjOne = {
+   id: 'about',
+   lightBg: false,
+   lightText: true,
+   lightTextDesc: false,
+   topLine: 'Premium Bank',
+   headline: 'Unlimited transactions with zero fees',
+   description: 'Get access to our exclusive App that allows you to send unlimited transactions without any fees',
+   buttonLabel: 'Get started',
+   imgStart: false,
+   img: require('../../images/svg-1.svg').default,
+   alt: 'Car',
+   dark: true,
+   primary: true,
+   darkText: false
+}
+
+export const homeObjTwo = {
+   id: 'discover',
+   lightBg: true,
+   lightText: false,
+   lightTextDesc: false,
+   topLine: 'Unlimited Access',
+   headline: 'Unlimited access to your hard-earned money',
+   description: 'You are the master of your money. Whenever you want it, you get it. No questions asked.',
+   buttonLabel: 'Learn More',
+   imgStart: true,
+   img: require('../../images/svg-2.svg').default,
+   alt: 'Piggy Bank',
+   dark: false,
+   primary: false,
+   darkText: true
+}
+
+export const homeObjThree = {
+   id: 'signup',
+   lightBg: true,
+   lightText: false,
+   lightTextDesc: false,
+   topLine: 'Join Our Team ',
+   headline: 'Creating an account is super simple',
+   description: 'Get everything set up and ready in under 10 minutes. All you need to do is add your information and you are ready to go',
+   buttonLabel: 'Start Now',
+   imgStart: false,
+   img: require('../../images/svg-3.svg').default,
+   alt: 'Paper',
+   dark: false,
+   primary: false,
+   darkText: true
+}
